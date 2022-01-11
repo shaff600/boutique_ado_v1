641aaf337f5c542a44d0529479bd91c67b749ef1
shaff600 <shafyl@live.co.uk>
2022-01-11 19:00:39 +0000

    added checkout view and template

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+from django.apps import AppConfig
+
+
+class CheckoutConfig(AppConfig):
+    name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
