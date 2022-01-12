05f2f681e3e95ac4716d7c65e10a5293cf1339b9
shaff600 <shafyl@live.co.uk>
2022-01-12 19:40:36 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KH51sBNpcSe4VAoYCXXhENYzGkQTwvXDbcATQY5S5becgwIySvwThl45tpkQhaKRyIreas3eQkgJKngXe9HU3Fz00dyu4ZaFT',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
